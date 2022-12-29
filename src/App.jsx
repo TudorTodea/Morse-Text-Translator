d0682d6eb2561c9a93b68cb21e8fbba67b5fac4c
TudorTodea <tudortodea1416@gmail.com>
2022-12-29 06:57:05 +0200

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import { Container } from '@mui/system'
 import Footer from './components/Footer/Footer'
 import Morse from './components/Morse/Morse'
 import AppNavbar from './components/Navbar/navbar'
-
+import { Routes, Route } from 'react-router-dom';
+import Learn from './components/Learn/Learn';
 
 
 function App() {
 
   return (
 <>
-
-   <Container maxWidth="lg" disableGutters sx={{ backgroundColor:'white', boxShadow: 3  }}>
-   <AppNavbar/>
-    <Morse/>
-    <Footer/>
+<Container maxWidth="lg" disableGutters sx={{ backgroundColor:'white', boxShadow: 3  }}>
+<AppNavbar/>
+<Routes>
+<Route path="/" element={<Learn/>} />
+<Route path="/translator" element={<Morse/>} />
+   </Routes>
+   <Footer/>
    </Container>
    </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { AudioPlayContextProvider } from './context/audio-context'
+import { BrowserRouter } from 'react-router-dom'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AudioPlayContextProvider>
+      <BrowserRouter>
     <App />
+    </BrowserRouter>
     </AudioPlayContextProvider>
   </React.StrictMode>,
 )
